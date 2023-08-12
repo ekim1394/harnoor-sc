d5eb051b002e05d4680a4a79d47e7879e518ae11
ekim <simplyeugene94@gmail.com>
2023-08-12 18:54:49 -0400

    making payment work

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Menu, X } from "react-feather"
 import {
   Container,
   Flex,
@@ -8,8 +7,6 @@ import {
   Space,
   NavLink,
   Button,
-  InteractiveIcon,
-  Nudge,
   VisuallyHidden,
 } from "./ui"
 import {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import * as React from "react"
 import SEOHead from "../components/head"
 import { graphql } from "gatsby"
-import { Container, Heading, WeeklyList, ButtonList, Subhead, Flex } from "../components/ui"
-import { Slice } from "gatsby"
+import * as ui from "../components/ui"
 import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
 
 
@@ -10,28 +9,64 @@ import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"
 export default function Schedule(props) {
     const { contentfulSchedule } = props.data
     const [checkedList, setCheckedList] = React.useState([]);
-    
+    const [campers, setCampers] = React.useState(0);
+    const [totalPrice, setTotalPrice] = React.useState(0)
+    const weeklyPrice = 50
+
     const handleSelect = (event) => {
+        console.log(event.target.checked)
         const value = event.target.name + ':' + event.target.value;
         const isChecked = event.target.checked;
-
+        var selected_list
         if (isChecked) {
             //Add checked item into checkList
-            const selected_list = [...checkedList, value]
-            selected_list.sort((a, b) => new Date(a.split(':')[1]) - new Date(b.split(':')[1]))
-            setCheckedList([...selected_list]);
+            selected_list = [...checkedList, value]
         } else {
             //Remove unchecked item from checkList
-            const filteredList = checkedList.filter((item) => item !== value);
-            filteredList.sort((a, b) => new Date(a.split(':')[1]) - new Date(b.split(':')[1]))
-            setCheckedList(filteredList);
+            selected_list = checkedList.filter((item) => item !== value);
 
         }
+        selected_list.sort((a, b) => new Date(a.split(':')[1]) - new Date(b.split(':')[1]))
+        setCheckedList([...selected_list]);
+        setTotalPrice(campers * selected_list.length * weeklyPrice)
     };
+
+    const handleChange = (event) => {
+        setCampers(event.target.value)
+        setTotalPrice(event.target.value * checkedList.length * weeklyPrice)
+    }
+
+    function createOrder(data, actions, err) {
+        return actions.order.create({
+            purchase_units: [
+                {amount: {value: totalPrice}}
+            ],
+            application_context: {
+                shipping_preference: 'NO_SHIPPING'
+            }
+        })
+    }
+
+    function onApprove(data, actions) {
+        // TODO Redirect and send email
+        return actions.order.capture().then(function (details) {
+            console.log(details.payer)
+            console.log(`Transaction completed by ${details.payer.name.given_name}!`)
+        });
+    }
+
+    function onError(err) {
+        console.log(err)
+    }
+
+    function onClick() {
+        console.log("When clicked, amount was", totalPrice);
+    }
+
 
     return (
         <>
-            <Container>
+            <ui.Container width="fullbleed">
                 {/* <FlexList>
                     {checkedList.map((item, index) => {
                         return (
@@ -39,38 +74,50 @@ export default function Schedule(props) {
                         );
                     })}
                 </FlexList> */}
-                <Heading center={true}>{contentfulSchedule.name}</Heading>
+                <ui.Heading center={true}>{contentfulSchedule.name}</ui.Heading>
+                <ui.Flex variant="center" responsive={true}>
+                    <h1># of campers: </h1>
+                    <input type="number" defaultValue={0} onChange={handleChange} />
+                </ui.Flex>
+                <ui.Flex variant="column">
+                    {
+                        contentfulSchedule.summerCampSessions.map(
+                            (dates) => {
+                                var startDateStr = new Date(dates.startDate).toString().substring(4, 10)
+                                var endDateStr = new Date(dates.endDate).toString().substring(4, 10)
+                                return (<ui.Container width='wide'>
+                                    <ui.Subhead center={true}>{startDateStr} - {endDateStr}</ui.Subhead>
+                                    <ui.WeeklyList name={dates.name} startDate={dates.startDate} endDate={dates.endDate} handleSelect={handleSelect} />
+                                    <br style={{ clear: 'both' }} />
+                                    <br style={{ clear: 'both' }} />
 
-                {
-                    contentfulSchedule.summerCampSessions.map(
-                        (dates) => {
-                            var startDateStr = new Date(dates.startDate).toString().substring(4, 10)
-                            var endDateStr = new Date(dates.endDate).toString().substring(4, 10)
-                            return (<Container width='wide'>
-                                <Subhead center={true}>{startDateStr} - {endDateStr}</Subhead>
-                                <WeeklyList name={dates.name} startDate={dates.startDate} endDate={dates.endDate} handleSelect={handleSelect} />
-                                <br style={{ clear: 'both' }} />
-                                <br style={{ clear: 'both' }} />
+                                </ui.Container>
+                                )
+                            }
+                        )
+                    }
+                </ui.Flex>
+                <ui.Subhead center={true}>Total Price: ${totalPrice}
+                </ui.Subhead>
 
-                            </Container>
-                            )
-                        }
-                    )
-                }
-                {checkedList.length > 0 &&
-                <Flex variant='center'>
-                    <PayPalScriptProvider options={
-                        {
+                {checkedList.length > 0 && totalPrice > 0 &&
+                    <ui.Flex variant='center'>
+                        <PayPalScriptProvider options={
+                            {
                                 clientId: "AfJI1B6OY9dOr4-tkGAnFdqEE96782Phgmw1I5XZ2ftHlBKNrBhse9ozglYUP303Fv3aOcS3AFXpH-YP",
-                            "enable-funding": "venmo"
-                        }
-                    }>
-                            <PayPalButtons style={{ color: "blue", shape: "pill", disableMaxWidth: false}} className="paypalButton"/>
-                    </PayPalScriptProvider>
-                </Flex>}
-            </Container>
+                                "enable-funding": "venmo",
+                            }
+                        }>
+                            <PayPalButtons style={{ color: "blue", shape: "pill", disableMaxWidth: false }} className="paypalButton"
+                            createOrder={createOrder}
+                            onApprove={onApprove}
+                            onError={onError}
+                            onClick={onClick}/>
+                        </PayPalScriptProvider>
+                    </ui.Flex>}
 
-            <Slice alias='footer' />
+
+            </ui.Container>
         </>
     )
 }
