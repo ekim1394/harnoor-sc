89ea611665747a90e99692fdbce99dcb7df3cb30
ekim <simplyeugene94@gmail.com>
2023-11-10 20:49:41 -0500

    removing cors stuff from email function

--- netlify/functions/email.ts
+++ netlify/functions/email.ts
@@ -8,42 +8,28 @@ exports.handler = async function (event, context, callback) {
     client.setApiKey(SENDGRID_API_KEY);
     console.log(event)
     
-    switch (event.httpMethod) {
-        case 'POST':
-            const data = {
-                from: 'info@physio-kids.com',
-                replyTo: 'info@physio-kids.com',
-                template_id: 'd-6ba6d50f1c564a8f8c6c17aba44039e7',
-                personalizations: [{
-                    to: recipient,
-                    dynamic_template_data: {
-                        "firstName": name,
-                        "formLink": "https://form.jotform.com/232527821060045"
-                    }
-                }]
-            };
-            try {
-                await client.send(data);
-                return {
-                    statusCode: 200,
-                    body: 'Message sent',
-                };
-            } catch (err) {
-                return {
-                    statusCode: err.code,
-                    body: JSON.stringify({ msg: err }),
-                };
+    const data = {
+        from: 'info@physio-kids.com',
+        replyTo: 'info@physio-kids.com',
+        template_id: 'd-6ba6d50f1c564a8f8c6c17aba44039e7',
+        personalizations: [{
+            to: recipient,
+            dynamic_template_data: {
+                "firstName": name,
+                "formLink": "https://form.jotform.com/232527821060045"
             }
-        default:
-            const headers = {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'POST'
-            };
-            return {
-                statusCode: 200,
-                headers: headers,
-                body: 'This was not a POST request!'
-            };
+        }]
+    };
+    try {
+        await client.send(data);
+        return {
+            statusCode: 200,
+            body: 'Message sent',
+        };
+    } catch (err) {
+        return {
+            statusCode: err.code,
+            body: JSON.stringify({ msg: err }),
+        };
     }
 };
