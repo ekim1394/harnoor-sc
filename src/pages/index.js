bf183a102ee674fe180156d5860061cb164d3c7a
ekim <simplyeugene94@gmail.com>
2023-11-11 14:17:22 -0500

    use current price

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,7 +101,7 @@ export default function Schedule(props) {
         return actions.order.create({
             purchase_units: [
                 { 
-                    amount: { value: 200 },
+                    amount: { value: priceRef.current },
                     payee: { email_address: "info@physiokids.com"}
                 }
             ],
