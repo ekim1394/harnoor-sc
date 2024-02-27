6c001c942f71fdf11d21c704d8a7d9001ec72090
Eugene Kim <simplyeugene94@gmail.com>
2024-02-26 20:57:07 -0500

    use proper naming conventions

--- src/service/sendgrid.service.ts
+++ src/service/sendgrid.service.ts
@@ -6,7 +6,7 @@ export function sendEmail(details, campersCnt, membershipSelected, weeks) {
         .post(`${window.location.href}.netlify/functions/email`, {
             recipient: details.payer.email_address,
             name: `${details.payer.name.given_name} ${details.payer.name.surname}`,
-            BCC_EMAIL,
+            bccEmail: BCC_EMAIL,
             camperCnt: campersCnt,
             membership: membershipSelected,
             weeks: weeks,
